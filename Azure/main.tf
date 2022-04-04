f1c1a0f82e7b3095491cafe17ae8f97d81e04980
Simon Anderson <simon.anderson111@gmail.com>
2022-04-04 11:50:03 +1000

    outputs and variables added

--- Azure/main.tf
+++ Azure/main.tf
@@ -15,14 +15,19 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "k8sResourceGroup" {
-  name     = "k8sResourceGroup"
-  location = "australiacentral2"
+  name     = var.resource_group_name
+  location = "australiasoutheast"
+
+  tags = {
+    "Environment" = "Tanzu Demo Environment"
+    "Team"        = "Tanzu Labs"
+  }
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "k8sVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "australiaeast"
+  location            = "australiasoutheast"
   resource_group_name = azurerm_resource_group.k8sResourceGroup.name
 
 }
