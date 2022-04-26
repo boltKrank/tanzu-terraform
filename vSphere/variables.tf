4307d4ece83dc7439e21eb3f3f4dbfb94113361d
Simon Anderson <simon.anderson111@gmail.com>
2022-04-27 09:08:47 +1000

    initial vpshere

---
+++ vSphere/variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  default = "user"
+}
+
+variable "vsphere_password" {
+  default = "uvsphere_passwordser"
+}
+
+variable "vsphere_server" {
+  default = "vsphere_server"
+}
