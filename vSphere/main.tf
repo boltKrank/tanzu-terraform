9e3550c51b81ac0235cf5f5d6a7d9e7cc398c751
Simon Anderson <simon.anderson111@gmail.com>
2022-04-27 13:24:21 +1000

    updated vpshere man and vars

--- vSphere/main.tf
+++ vSphere/main.tf
@@ -7,40 +7,42 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-# data "vsphere_datacenter" "dc" {
-#   name = "dc1"
-# }
+# Will use var.vsphere_datacenter
+data "vsphere_datacenter" "dc" {
+   name = var.vsphere_datacenter_name
+}
 
-# data "vsphere_datastore" "datastore" {
-#   name          = "datastore1"
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
+data "vsphere_datastore" "datastore" {
+   name          = var.vsphere_datastore_name
+   datacenter_id = data.vsphere_datacenter.dc.id
+}
 
-# data "vsphere_resource_pool" "pool" {
-#   name          = "cluster1/Resources"
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
+data "vsphere_resource_pool" "pool" {
+   name          = var.vsphere_resource_pool_name
+   datacenter_id = data.vsphere_datacenter.dc.id
+}
 
-# data "vsphere_network" "network" {
-#   name          = "public"
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
+data "vsphere_network" "network" {
+  name          = var.vsphere_network_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
 
-# resource "vsphere_virtual_machine" "vm" {
-#   name             = "terraform-test"
-#   resource_pool_id = data.vsphere_resource_pool.pool.id
-#   datastore_id     = data.vsphere_datastore.datastore.id
+# TODO: Change this to a template
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vsphere_virtual_machine_name
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
-#   num_cpus = 2
-#   memory   = 1024
-#   guest_id = "other3xLinux64Guest"
+  num_cpus = 2
+  memory   = 1024
+  guest_id = var.vsphere_vm_guest_id
 
-#   network_interface {
-#     network_id = data.vsphere_network.network.id
-#   }
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
 
-#   disk {
-#     label = "disk0"
-#     size  = 20
-#   }
-# }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
--- vSphere/variables.tf
+++ vSphere/variables.tf
@@ -9,3 +9,27 @@ variable "vsphere_password" {
 variable "vsphere_server" {
   default = "vsphere_server"
 }
+
+variable "vsphere_datacenter_name" {
+  default = "vsphere_datacenter_name"
+}
+
+variable "vsphere_resource_pool_name" {
+  default = "vsphere_resource_pool_name"
+}
+
+variable "vsphere_network_name" {
+  default = "vsphere_network_name"
+}
+
+variable "vsphere_datastore_name" {
+  default = "vsphere_datastore_name"
+}
+
+variable "vsphere_virtual_machine_name" {
+  default = "vsphere_virtual_machine_name"
+}
+
+variable "vsphere_vm_guest_id" {
+  default = "vsphere_vm_guest_id"
+}
