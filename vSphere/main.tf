73bebde5144fc7ce2dfb98360feff958b79f7cc6
Simon Anderson <simon.anderson111@gmail.com>
2022-04-27 16:55:38 +1000

    s

--- vSphere/main.tf
+++ vSphere/main.tf
@@ -17,7 +17,7 @@ data "vsphere_datastore" "datastore" {
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_resource_pool" "pool" {
+data "vsphere_resource_pool" "pool" { 
    name          = var.vsphere_resource_pool_name
    datacenter_id = data.vsphere_datacenter.dc.id
 }
